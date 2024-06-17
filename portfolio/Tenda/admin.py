4a30d9fbe5b354969bdf98bb7f1110ba3ced7216
Pharel Carter <Pharelcarter@gmail.com>
2024-06-17 22:57:32 +0200

    Pharel customized the admin site

--- portfolio/Tenda/admin.py
+++ portfolio/Tenda/admin.py
@@ -3,7 +3,22 @@ from .models import *
 # Register your models here.
 
 admin.site.register(User)
-admin.site.register(TendaProject)
-admin.site.register(TendaSkill)
+# admin.site.register(TendaProject)
+# admin.site.register(TendaSkill)
 
+@admin.register(TendaProject)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'description', 'techonogies_use')
+    search_fields = ('description', 'title')
+    list_filter = ('title',  'user')
+
+@admin.register(TendaSkill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'category')
+    search_fields = ('name', 'category')
+    list_filter = ('name',  'user')
+
+admin.site.site_header = "Portfolio Administration"
+admin.site.site_title = "Portfolio Admin Portal"
+admin.site.index_title = "Welcome to Portfolio Administration"
 
